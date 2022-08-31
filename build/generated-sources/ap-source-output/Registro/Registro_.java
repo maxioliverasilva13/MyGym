d81268c1864c9f411cc2574ae221ee598291c769
Maximiliano Olivera Silva <maximiliano.olivera@dualbootpartners.com>
2022-08-31 15:26:14 -0300

    my changes

--- build/generated-sources/ap-source-output/Registro/Registro_.java
+++ build/generated-sources/ap-source-output/Registro/Registro_.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2022-08-29T00:11:53", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2022-08-31T00:06:14", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Registro.class)
 public class Registro_ { 
 
